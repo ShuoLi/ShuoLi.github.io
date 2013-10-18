9e658fdbe4024c2dc9cf9a6da0798e33fb4a7c96
ShuoLi <dantein88@gmail.com>
2013-10-18 15:37:55 -0400

    homepage refreshed, css conflict in img field

--- AngularAnimationTest/animation.js
+++ AngularAnimationTest/animation.js
@@ -1,3 +1,17 @@
 /**
  * Created by sli on 10/18/13.
  */
+var app = angular.module('myApp', ['ngAnimate']);
+
+app.controller("MyController",function($scope){
+    $scope.shouldShowHello=false;
+
+    $scope.showHello = function(){
+        $scope.shouldShowHello= true;
+    };
+
+    $scope.reset = function(){
+        $scope.shouldShowHello = false;
+    }
+});
+
